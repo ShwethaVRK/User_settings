dcfef3c389c4572b94cc6dc652cd6a55c2290954
Not Shwetha <vrkshwethav@gmail.com>
2019-01-31 15:11:31 +0530

    Commit

---
+++ languages/en.php
@@ -0,0 +1,19 @@
+<?php
+return [
+
+	'user:settings:show_statistics' => 'Show Statistics',
+	'user:settings:show_language' => 'Show Language Setting',
+
+	'user:settings:profile' => 'Profile',
+	'user:settings:avatar' => 'Avatar',
+	'user:settings:account' => 'Account',
+	'user:settings:tools' => 'Tools',
+	'user:settings:tools:no_results' => 'There are no tool that require extra configuration',
+	'user:settings:notifications' => 'Notifications',
+	'user:settings:statistics' => 'Statistics',
+
+	'notifications:subscriptions:friends:title' => 'Collections',
+	'notifications:subscriptions:friends:description' => 'Below you can subscribe to notifications from collections of users. You can further opt in or opt out from receiving notifications about individual users within each collection. If you subscribe to an entire collection, your preferences will apply to new users added to the collection in the future.',
+	'notifications:users:all' => 'All subscriptions',
+];
+
---
+++ release.md
@@ -0,0 +1,1 @@
+Self-contained .zip file that includes all plugin dependencies.
